6ca3d8988e83b997bde80bee50901f53d0662e17
Jaime Castro <jandrescastro97@gmail.com>
2021-08-02 14:51:25 -0500

    img pokemons

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -6,38 +6,63 @@ const getData = (api) => {
     return fetch(api)
     .then((response) => response.json())
     .then((json) => {
-        llenarDatos(json.results), paginacion(json.next,json.previous);
+        pokeData(json.results),paginacion(json);
     })
     .catch((error) => {
         console.log("Error", error);
     });
 };
 
-//Dibujar cards de personajes
-const llenarDatos = (data) => {
+// const pokeData
+
+const pokeData = (data) => {
     let html = "";
-    data.forEach((pj) => {
-        html += '<div class="col mt-5">';
-        html += '<div class="card" style="width: 10rem;">';
-        html += '<div class="card-boddy">';
-        html += `<h5 class="card-tittle">${pj.name}</h5>`;
-        html += "</div>";
-        html += "</div>";
-        html += "</div>";
+    document.getElementById("datosPersonajes").innerHTML = "";
+    data.forEach((pj) =>{
+        const URL = pj.url;
+        return fetch(URL)
+        .then((response) => response.json())
+        .then((json) => {
+            llenarDatos(json, html);
+    })
+    .catch((error) => {
+        console.log("Error", error);
     });
-    document.getElementById("datosPersonajes").innerHTML = html;
+        
+    });
+}
+
+//Dibujar cards de personajes
+const llenarDatos = (data, html) => {
+
+
+    html += '<div class="col mt-5">';
+    html += '<div class="card" style="width: 10rem;">';
+    html += `<img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="${data.name}">`;
+    html += '<div class="card-body">';
+    html += `<h5 class="card-title">${data.name}</h5>`;
+    html += `<p class="card-text">Status: ${data.weight}</p>`;
+    html += `<p class="card-text">Specie: ${data.height}</p>`;
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+    
+    document.getElementById("datosPersonajes").innerHTML += html;
 };
 
-//Paginacion
-const paginacion = (datanext,dataprevious) => {
+// paginacion
+const paginacion = (data) => {
     let prevDisabled = "";
     let nextDisabled = "";
   
-    let html = `<li class="page-item ${dataprevious == null?prevDisabled = "disabled":prevDisabled = ""}"><a class="page-link" onclick="getData('${dataprevious}')">Previous</a></li> <li class="page-item ${ datanext == null?nextDisabled = "disabled":nextDisabled = ""}" ><a class="page-link" onclick="getData('${datanext}')">Next</a></li>`;
-   
+    data.previous == null ? (prevDisabled = "disabled") : (prevDisabled = "");
+  data.next == null ? (nextDisabled = "disabled") : (nextDisabled = "");
+
+  let html = `<li class="page-item ${prevDisabled}" ><a class="page-link" onclick="getData('${data.previous}')">Previous</a></li> <li class="page-item ${nextDisabled}" ><a class="page-link" onclick="getData('${data.next}')">Next</a></li>`;
+
+  document.getElementById("paginacion").innerHTML = html;
   
-    document.getElementById("paginacion").innerHTML = html;
-};
+  };
 
 // Se ejecuta la API
 getData(API);
